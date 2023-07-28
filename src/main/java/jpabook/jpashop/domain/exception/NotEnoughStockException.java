8d28ed77d8097e782aac1b684d7878ae710c0a00
newdana01 <newdana01@gmail.com>
2023-07-28 14:37:04 +0900

    NotEnoughStockException 클래스 추가

---
+++ src/main/java/jpabook/jpashop/domain/exception/NotEnoughStockException.java
@@ -0,0 +1,18 @@
+ package jpabook.jpashop.domain.exception;
+
+public class NotEnoughStockException extends RuntimeException {
+    public NotEnoughStockException() {
+        super();
+    }
+
+    public NotEnoughStockException(String message) {
+    }
+
+    public NotEnoughStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughStockException(Throwable cause) {
+        super(cause);
+    }
+}
