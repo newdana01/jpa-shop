a7dec09284f253b9c0e52ecf06adca9e10307085
newdana01 <newdana01@gmail.com>
2023-07-31 11:57:00 +0900

    MemberForm 클래스 추가

---
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop.controller;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MemberForm {
+    private String name;
+
+    @NotEmpty(message = "회원 이름은 필수 입니다")
+    private String city;
+    private String zipCode;
+    private String street;
+}
