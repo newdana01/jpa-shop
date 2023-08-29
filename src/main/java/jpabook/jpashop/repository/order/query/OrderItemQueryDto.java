bd9f30aebbcbd54846513c1fad02507859573b9d
newdana01 <newdana01@gmail.com>
2023-08-29 11:00:20 +0900

    ordersV6 추가

--- src/main/java/jpabook/jpashop/repository/order/query/OrderItemQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderItemQueryDto.java
@@ -1,9 +1,13 @@
 package jpabook.jpashop.repository.order.query;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(of = "orderId")
 public class OrderItemQueryDto {
+    @JsonIgnore
     private Long orderId;
     private String itemName;
     private int count;
